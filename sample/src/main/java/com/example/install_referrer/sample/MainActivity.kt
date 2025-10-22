8a69f97f9253f9e32df9f285ccf033d3038eb237
Khoi Le <khoiln218@gmail.com>
2025-10-22 10:02:16 +0700

    implement sample

--- sample/src/main/java/com/example/install_referrer/sample/MainActivity.kt
+++ sample/src/main/java/com/example/install_referrer/sample/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.install_referrer.sample
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageInfo
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.inetkr.install_referrer.InstallReferrer
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +22,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val packageInfo = packageManager.getPackageInfo(packageName, 0)
+        findViewById<TextView>(R.id.packageName).text = packageName
+        findViewById<TextView>(R.id.app_version).text =
+            "App version ${packageInfo.versionName}+${getLongVersionCode(packageInfo)}, plugin ${
+                InstallReferrer.getInstance(
+                    this
+                ).getVersion()
+            }"
         InstallReferrer.getInstance(this).initialize(debug = true)
+    }
+
+    @Suppress("deprecation")
+    private fun getLongVersionCode(info: PackageInfo): Long {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            info.longVersionCode
+        } else {
+            info.versionCode.toLong()
+        }
     }
 }
